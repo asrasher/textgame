c8ad48a241a059c24ec1ff04ddded65adcf0fe04
ANNA ASHER <anna.asher@fdmgroup.com>
2023-08-03 11:32:57 +0100

    first commit

---
+++ src/test/java/com/textgame/textgame/TextgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.textgame.textgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
